195d1342565d54c73da73e2ff917ea1f4ef3dde1
Julien Guibert-Peeters <julien@mountainhacks.org>
2026-02-13 18:56:50 +0100

    feat: expose console writer as erased type

--- noshell/src/console.rs
+++ noshell/src/console.rs
@@ -3,8 +3,10 @@
 use futures::Stream;
 
 use heapless::{String, Vec};
+use noterm::cursor::Home;
 use noterm::events::Event;
-use noterm::io;
+use noterm::terminal::{Clear, ClearType};
+use noterm::{Queuable, io};
 
 use crate::line;
 use crate::line::Prompt;
@@ -37,30 +39,43 @@ const LINE_BUFFER_CAPACITY: usize = 256;
 const ARGV_BUFFER_CAPACITY: usize = 32;
 
 /// A console.
-pub struct Console<'a, EventsTy, OutputTy> {
+pub struct Console<'a, EventsTy, WriterTy> {
     prompt: Prompt<'a>,
     events: EventsTy,
-    output: OutputTy,
+    writer: WriterTy,
 }
 
-impl<'a, EventsTy, OutputTy> Console<'a, EventsTy, OutputTy>
+impl<'a, EventsTy, WriterTy> Console<'a, EventsTy, WriterTy>
 where
     EventsTy: Stream<Item = io::Result<Event>> + Unpin,
-    OutputTy: io::blocking::Write,
+    WriterTy: io::blocking::Write,
 {
     /// Create a new console.
-    pub fn new(events: EventsTy, output: OutputTy) -> Self {
+    pub fn new(events: EventsTy, output: WriterTy) -> Self {
         Console {
             prompt: Prompt::new("shell $"),
             events,
-            output,
+            writer: output,
         }
+    }
+
+    /// Clear the console.
+    pub fn clear(&mut self) -> Result<()> {
+        self.writer.queue(Clear(ClearType::All))?;
+        self.writer.queue(Home)?;
+        self.writer.flush()?;
+        Ok(())
+    }
+
+    /// Get output writer.
+    pub fn writer<'b>(&'b mut self) -> ConsoleWriter<'b> {
+        ConsoleWriter(&mut self.writer)
     }
 
     /// Process user input.
     pub async fn process(&mut self) -> Result<()> {
         let line: String<LINE_BUFFER_CAPACITY> =
-            match line::readline(&self.prompt, &mut self.events, &mut self.output).await {
+            match line::readline(&self.prompt, &mut self.events, &mut self.writer).await {
                 Ok(line) => line,
 
                 Err(line::Error::Io(err)) => return Err(Error::Io(err)),
@@ -68,6 +83,8 @@ where
                 Err(line::Error::NoSpaceLeft) => return Err(Error::NoSpaceLeft),
                 Err(line::Error::Unknown) => return Err(Error::Unknown),
             };
+
+        noterm::print!(&mut self.writer, "\r\n");
 
         // Skip comment.
         if line.starts_with('#') {
@@ -79,7 +96,7 @@ where
             Ok(argv) => argv,
 
             Err(line::lexer::Error::InvalidInput(failed)) => {
-                noterm::println!(&mut self.output, "error: unexpected token `{}`", failed);
+                noterm::println!(&mut self.writer, "error: unexpected token `{}`", failed);
                 return Ok(());
             }
             Err(line::lexer::Error::Unknown) => return Err(Error::Unknown),
@@ -91,7 +108,36 @@ where
 
     /// Execute the command.
     pub(crate) async fn execute(&mut self, argv: &[&str]) -> Result<()> {
-        noterm::println!(&mut self.output, "execute `{}`!", argv.join(" "));
+        handler(argv, &mut self.writer());
         Ok(())
+    }
+}
+
+fn handler(argv: &[&str], writer: &mut ConsoleWriter<'_>) {
+    noterm::println!(writer, "execute `{:?}`!", argv);
+}
+
+/// Console writer.
+pub struct ConsoleWriter<'a>(&'a mut dyn io::blocking::Write);
+
+impl ConsoleWriter<'_> {
+    /// Clear the console output.
+    pub fn clear(&mut self) -> Result<()> {
+        use io::blocking::Write;
+
+        self.queue(Clear(ClearType::All))?;
+        self.queue(Home)?;
+        self.flush()?;
+        Ok(())
+    }
+}
+
+impl io::blocking::Write for ConsoleWriter<'_> {
+    fn write(&mut self, data: &[u8]) -> io::Result<usize> {
+        self.0.write(data)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.flush()
     }
 }
